9792265f4eb68987cd65e05f261a6fef22eed607
Dmitry <17dx@ro.ru>
2017-10-07 20:25:14 +0300

    sharememPOSIX_fix

--- shm_POSIX.cpp
+++ shm_POSIX.cpp
@@ -5,7 +5,7 @@
 #include <fcntl.h>
 #include <sys/mman.h>
 #include <unistd.h> // ftruncate
-
+// -lrt
 
 const int MEM_SIZE = 1024*1024;
 int main()
@@ -13,7 +13,7 @@ int main()
     const char * shm_name = "/test.shm";
     
     
-    int shm_d = shm_open(shm_name, O_CREAT, 0777 );         
+    int shm_d = shm_open(shm_name, O_CREAT|O_RDWR, 0777 );         
     if ( shm_d == -1 ){
         perror("shm_open");
         return 1;
