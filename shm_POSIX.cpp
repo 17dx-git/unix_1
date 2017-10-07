621ca772553d82cf76c595662b6504b26aa7a860
Dmitry <17dx@ro.ru>
2017-10-07 20:17:51 +0300

    sharememPOSIX

---
+++ shm_POSIX.cpp
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h> //для memset
+#include <sys/types.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <unistd.h> // ftruncate
+
+
+const int MEM_SIZE = 1024*1024;
+int main()
+{
+    const char * shm_name = "/test.shm";
+    
+    
+    int shm_d = shm_open(shm_name, O_CREAT, 0777 );         
+    if ( shm_d == -1 ){
+        perror("shm_open");
+        return 1;
+    }
+    
+    if ( ftruncate(shm_d, MEM_SIZE ) == -1){
+        perror("ftruncate");
+        return 1;
+    }
+    
+    int prot = (PROT_READ | PROT_WRITE);
+    int flags = MAP_SHARED;
+    void *sh_mem = mmap(NULL, MEM_SIZE, prot, flags, shm_d, 0);
+    if (sh_mem == (void *)-1){
+        perror("mmap");
+        return 1;
+    }                  
+                  
+    memset (sh_mem, 13, MEM_SIZE);
+       
+    return 0;
+
+}
